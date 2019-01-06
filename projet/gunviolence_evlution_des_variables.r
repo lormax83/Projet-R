a2b3265b732e544670634708bb0f01b0895dc011
Maxime M <maxime.moineau@free.fr>
2019-01-06 17:56:40 +0100

    j'avais pas prévu de devoir reset pour enlever le dataset...
    plz work
    message du dernier commit perdu : en dur ca marche mais dans une boucle interroger le dataset ça revoie n'importe quoi

--- projet/gunviolence_evlution_des_variables.r
+++ projet/gunviolence_evlution_des_variables.r
@@ -1,5 +1,48 @@
+print("Projet de R")
+
+#on reset la mémoire
+
+rm(list = ls())
+
+print("Déclarations de fonctions")
+
+filterInterestingData <- function(allData) {
+  filteredData <- matrix(ncol=5)
+  colnames(filteredData) <- c("nombre_morts", "nombre_blesses", "nombre_de_malfaiteurs", "age_moyen_des_malfaiteurs", "etat")
+  for(row in 1:nrow(allData)){
+    if(length(allData[row, "date"])>0){
+
+      #on a un row a priori renseigné
+      nombre_morts <- allData[row, "n_killed"]
+      nombre_blesses <- allData[row, "n_injured"]
+      etat <- allData[row, "state"]
+      
+      nombre_de_malfaiteurs <- "todo"
+      age_moyen_des_malfaiteurs <- "todo"
+      
+      aRow <- c(nombre_morts, nombre_blesses, nombre_de_malfaiteurs, age_moyen_des_malfaiteurs, etat)
+      rbind(filteredData,aRow, NULL) -> filteredData
+      
+      print(paste(aRow, "\n" , row, "/", nrow(allData)))
+    }
+    if(row>3) return(filteredData) # for testing
+  }
+  return(filteredData)
+}
+# POINT D'ENTREE
+
+#on se place dans l'arboresence de fichier a l'endroit ou est le projet
 getwd()
-rm(allData)
-allData <- read.table("ressources/gun-violence-data_01-2013_03-2018.csv", sep = ";", header=FALSE, fill=TRUE)
-# allDataContient toutes les sous parties des données sur lesquelles on va faire des stats
+setwd("/home/asus/Desktop/work/M1_MATHS/Projet-R")
+print("on recupere le dataset avec les entetes")
+
+headers <- c("incident_id","date","state","city_or_county","address","n_killed","n_injured","incident_url","source_url","incident_url_fields_missing","congressional_district","gun_stolen","gun_type","incident_characteristics","latitude","location_description","longitude","n_guns_involved","notes","participant_age","participant_age_group","participant_gender","participant_name","participant_relationship","participant_status","participant_type","sources","state_house_district","state_senate_district")
+allData <- read.table("ressources/gun-violence-data_01-2013_03-2018.csv", sep = ",", header=FALSE, skip = 1, skipNul = TRUE, fill = TRUE, quote = "", strip.white = TRUE )
+colnames(allData) <- headers
+
+#View(allData)
+print("On recupere les donnes qui nous interessent depuis le dataset")
+
+filteredDataRes <- filterInterestingData(allData)
+View(filteredDataRes)
 
