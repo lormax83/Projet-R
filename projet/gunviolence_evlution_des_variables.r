e05537bb48cee3ade5181d78bd10ee383e99438d
Maxime M <maxime.moineau@free.fr>
2019-01-07 22:41:37 +0100

    Error in filteredData[, "date"] <- unlist(datelist, use.names = FALSE) :    le nombre d'objets à remplacer n'est pas multiple de la taille du remplacement

--- projet/gunviolence_evlution_des_variables.r
+++ projet/gunviolence_evlution_des_variables.r
@@ -1,25 +1,37 @@
+library("data.table") # install.packages("data.table")
 print("Projet de R")
 
 #on reset la mémoire
 
 rm(list = ls())
 
+
 print("Déclarations de fonctions")
+
+get_years <- function(date) {
+  temp<-substr(date,1,4)
+  if(is.null(temp))
+  if(is.numeric(temp)&as.numeric(temp)>1900)
+    return(temp)
+  else return(NULL)
+}
 
 filterInterestingData <- function(allData) {
   print("filterInterestingData")
   filteredData <- matrix(ncol=4)
   colnames(filteredData) <- c("date", "nombre_morts", "nombre_blesses", "etat")
   for(row in 0:nrow(allData)){
+    date <- get_years(allData[row, "date"])
+    
     if(
-       is.null(allData[row, "date"]) | length(allData[row, "date"])==0 |
+       is.null(date)|
        is.null(allData[row, "n_killed"]) | length(allData[row, "n_killed"])==0 |
        is.null(allData[row, "n_injured"]) | length(allData[row, "n_injured"])==0 |
        is.null(allData[row, "state"]) | length(allData[row, "state"])==0
        ){ 
       next
     }
-      date <- allData[row, "date"] 
+      
       #on saute les rows incohérents 
       nombre_morts <- allData[row, "n_killed"]
       nombre_blesses <- allData[row, "n_injured"]
@@ -28,9 +40,7 @@ filterInterestingData <- function(allData) {
       #nombre_de_malfaiteurs <- "NULL" # On ne peut pas obtenir cette donnée car la collection est trop hétérogène et parfois incohérente
       #age_moyen_des_malfaiteurs <- "NULL" # On ne peut pas obtenir cette donnée car la collection est trop hétérogène et parfois incohérente
       aRow <- tryCatch({
-        dateAsDate <- as.Date(date, "%Y-%m-%d", "%Y/%m/%d",FALSE)
-        if(is.na(dateAsDate)) next
-        c(paste("",dateAsDate), nombre_morts, nombre_blesses, etat) # nombre_de_malfaiteurs, age_moyen_des_malfaiteurs retirés
+        c(paste("",date), nombre_morts, nombre_blesses, etat) # nombre_de_malfaiteurs, age_moyen_des_malfaiteurs retirés
       }, warning = function(w) {
         return(NULL);
       }, error = function(e) {
@@ -48,16 +58,23 @@ filterInterestingData <- function(allData) {
    
     if(row>500) return(filteredData) # faster for testing
   }
-  print("***")
-  print(class(filteredData))
-  print("***")
+  datelist <- lapply(X = filteredData[,"date"], FUN = get_years)
+  print(class(datelist))
+  nombre_mortslist <- lapply(X = filteredData[,"nombre_morts"], FUN = as.numeric )
+  print(class(nombre_mortslist))
+  nombre_blesseslist <- lapply(X = filteredData[,"nombre_blesses"], FUN = as.numeric )
+  print(class(nombre_blesseslist))
+  etatlist <- lapply(X = filteredData[,"etat"], FUN = as.character)
+  print(class(etatlist))
+  
+  filteredData[,"date"] <- unlist(datelist, use.names = FALSE)
+  filteredData[,"nombre_morts"] <- unlist(nombre_mortslist, use.names = FALSE)
+  filteredData[,"nombre_blesses"] <- unlist(nombre_blesseslist, use.names = FALSE)
+  filteredData[,"etat"] <- unlist(etatlist, use.names = FALSE)
+  
   return(filteredData)
 }
 
-get_years <- function(date) {
-  posixDate <- as.POSIXct(date,format="%Y/%m/%d",tz="America/Chicago")
-  return(year(as.Date(posixDate)))
-}
 # POINT D'ENTREE
 
 #on se place dans l'arboresence de fichier a l'endroit ou est le projet
@@ -66,17 +83,16 @@ setwd("/home/asus/Desktop/work/M1_MATHS/")
 print("on recupere le dataset avec les entetes")
 
 headers <- c("incident_id","date","state","city_or_county","address","n_killed","n_injured","incident_url","source_url","incident_url_fields_missing","congressional_district","gun_stolen","gun_type","incident_characteristics","latitude","location_description","longitude","n_guns_involved","notes","participant_age","participant_age_group","participant_gender","participant_name","participant_relationship","participant_status","participant_type","sources","state_house_district","state_senate_district")
-allData <- read.csv("gun-violence-data_01-2013_03-2018.csv", sep = ",", header=FALSE, skip = 1, skipNul = TRUE, fill = TRUE, quote = "", strip.white = TRUE )
+allData <- fread("gun-violence-data_01-2013_03-2018.csv", sep = ",", header=TRUE, nrows = 500)
+allData <- as.data.frame(allData)
 colnames(allData) <- headers
 
 #View(allData)
 print("On recupere les donnes qui nous interessent depuis le dataset")
 filteredDataRes <- filterInterestingData(allData)
 
-lapply(filteredDataRes[0], get_years)
-lapply(filteredDataRes[1], as.numeric )
-lapply(filteredDataRes[2], as.numeric )
-lapply(filteredDataRes[3], as.character)
+
+print(typeof(filteredDataRes))
 
 View(filteredDataRes)
 
