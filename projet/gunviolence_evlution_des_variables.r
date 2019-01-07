b88b2a2f44251e865cb819e977036e1166285b41
Maxime M <maxime.moineau@free.fr>
2019-01-07 15:27:30 +0100

    wow il me renvoie un truc cohérent
    reste plus qu'a le mettre dans une table puis dans un graphe

--- projet/gunviolence_evlution_des_variables.r
+++ projet/gunviolence_evlution_des_variables.r
@@ -16,14 +16,14 @@ filterInterestingData <- function(allData) {
       nombre_morts <- allData[row, "n_killed"]
       nombre_blesses <- allData[row, "n_injured"]
       etat <- allData[row, "state"]
-      
+      print(paste("morts :", nombre_morts, ", blesses :", nombre_blesses, "etat :", etat, "\n"))
       nombre_de_malfaiteurs <- "todo"
       age_moyen_des_malfaiteurs <- "todo"
       
       aRow <- c(nombre_morts, nombre_blesses, nombre_de_malfaiteurs, age_moyen_des_malfaiteurs, etat)
       rbind(filteredData,aRow, NULL) -> filteredData
       
-      print(paste(aRow, "\n" , row, "/", nrow(allData)))
+      
     }
     if(row>3) return(filteredData) # for testing
   }
@@ -33,11 +33,11 @@ filterInterestingData <- function(allData) {
 
 #on se place dans l'arboresence de fichier a l'endroit ou est le projet
 getwd()
-setwd("/home/asus/Desktop/work/M1_MATHS/Projet-R")
+setwd("/home/asus/Desktop/work/M1_MATHS/")
 print("on recupere le dataset avec les entetes")
 
 headers <- c("incident_id","date","state","city_or_county","address","n_killed","n_injured","incident_url","source_url","incident_url_fields_missing","congressional_district","gun_stolen","gun_type","incident_characteristics","latitude","location_description","longitude","n_guns_involved","notes","participant_age","participant_age_group","participant_gender","participant_name","participant_relationship","participant_status","participant_type","sources","state_house_district","state_senate_district")
-allData <- read.table("ressources/gun-violence-data_01-2013_03-2018.csv", sep = ",", header=FALSE, skip = 1, skipNul = TRUE, fill = TRUE, quote = "", strip.white = TRUE )
+allData <- read.csv("gun-violence-data_01-2013_03-2018.csv", sep = ",", header=FALSE, skip = 1, skipNul = TRUE, fill = TRUE, quote = "", strip.white = TRUE )
 colnames(allData) <- headers
 
 #View(allData)
