d1841eeb371ccf9a04d101d5523cb86c7bc18b26
Maxime M <maxime.moineau@free.fr>
2019-01-08 01:11:45 +0100

    surrender

--- projet/gunviolence_evlution_des_variables.r
+++ projet/gunviolence_evlution_des_variables.r
@@ -1,4 +1,5 @@
 print("Projet de R")
+setwd("/home/asus/Desktop/work/M1_MATHS/")#change me plz
 
 #on reset la mémoire
 
@@ -62,11 +63,11 @@ filterInterestingData <- function(allData) {
 
 #on se place dans l'arboresence de fichier a l'endroit ou est le projet
 getwd()
-setwd("/home/asus/Desktop/work/M1_MATHS/")
+
 print("on recupere le dataset avec les entetes")
 
 headers <- c("incident_id","date","state","city_or_county","address","n_killed","n_injured","incident_url","source_url","incident_url_fields_missing","congressional_district","gun_stolen","gun_type","incident_characteristics","latitude","location_description","longitude","n_guns_involved","notes","participant_age","participant_age_group","participant_gender","participant_name","participant_relationship","participant_status","participant_type","sources","state_house_district","state_senate_district")
-allData <- fread("gun-violence-data_01-2013_03-2018.csv", sep = ",", header=TRUE, nrows = 200)
+allData <- fread("gun-violence-data_01-2013_03-2018.csv", sep = ",", header=TRUE, nrows = 2000)
 allData <- as.data.frame(allData)
 colnames(allData) <- headers
 
@@ -84,14 +85,21 @@ etatList <- filteredDataRes[, "etat"]
 #barplot(dateList , col="orange" ,names.arg=etatList, main = "crimes par etat")
 
 mortList.mean <- mean( mortList, na.rm=TRUE )
-print(mortList.mean)
+#print(mortList.mean)
 mortList.sd <- sd( mortList, na.rm=TRUE ) 
-print(mortList.sd)
+#print(mortList.sd)
 mortList.norm <- pnorm( mortList, m=mortList.mean, sd=mortList.sd )
-print(mortList.norm)
+#print(mortList.norm)
 mortList.density <- density(mortList.norm, na.rm=TRUE)
-plot(mortList.density, main="Fonction densité")
+plot(mortList.density, main="Fonction densité du nombre de morts")
 
+hist(mortList, breaks=50 , xlim=c(0,5), border=F, col=rgb(0.1,0.8,0.3,0.5), ylab="Fréquence" ,xlab="Valeurs de mortList" , main="Distribution de la variable du nombre de mort par balle")
+hist(blessesList, breaks=50 , xlim=c(0,5), border=F, col=rgb(0.1,0.8,0.3,0.5), ylab="Fréquence" ,xlab="Valeurs de blessesList" , main="Distribution de la variable du nombre de blessés par balle")
+nbCrimesParPays<-rle(sort(etatList))
+print(nbCrimesParPays)
+#print( length(unlist(nbCrimesParPays[1])) )
+#xnbCrimesParPays <- matrix(as.numeric(unlist(nbCrimesParPays["lengths"])), nc=1, nr=length(unlist(nbCrimesParPays[2])), byrow=T)
+#barplot(xnbCrimesParPays[1],1, names.arg=xnbCrimesParPays[2], space = TRUE, xlab="state", ylab="crimes")
 
 print("TODO On établit des corrélations sur ces données")
 
